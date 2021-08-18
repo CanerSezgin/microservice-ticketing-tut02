37564cdb42d0acd9633c0374bb77597774981020
Caner Sezgin <ccanersezgin@gmail.com>
2021-08-18 13:53:37 +0300

    client auth pages done

--- client/pages/_app.js
+++ client/pages/_app.js
@@ -1,5 +1,33 @@
 import 'bootstrap/dist/css/bootstrap.css';
+import buildClient from '../api/build-client';
+import Header from '../components/header';
 
-export default ({ Component, pageProps }) => {
-  return <Component {...pageProps} />;
+const AppComponent = ({ Component, pageProps, currentUser }) => {
+  return (
+    <div>
+      <Header currentUser={currentUser} />
+      <Component {...pageProps} currentUser={currentUser} />;
+    </div>
+  );
 };
+
+AppComponent.getInitialProps = async (appContext) => {
+  console.log('APP GET_INIT_PROPS');
+  const client = buildClient(appContext.ctx);
+
+  const r = await client
+    .get('/api/users/currentuser')
+    .catch((e) => ({ data: {} }));
+  const { currentUser } = r.data;
+
+  let pageProps = {};
+  if (appContext.Component.getInitialProps) {
+    pageProps = await appContext.Component.getInitialProps(appContext.ctx);
+  }
+  return {
+    currentUser,
+    pageProps,
+  };
+};
+
+export default AppComponent;
--- client/pages/index.js
+++ client/pages/index.js
@@ -1,22 +1,9 @@
-import buildClient from '../api/build-client';
-
 const LandingPage = ({ currentUser }) => {
   return currentUser ? (
     <h1>You are signed in</h1>
   ) : (
     <h1>You are NOT signed in</h1>
   );
-};
-
-// Fetch Data during SSR
-LandingPage.getInitialProps = async (context) => {
-  const client = buildClient(context);
-  try {
-    const { data } = await client.get('/api/users/currentuser');
-    return data;
-  } catch (error) {
-    return { error };
-  }
 };
 
 export default LandingPage;
